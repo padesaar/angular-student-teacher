06eee76da3c62461cd184489ef161351262f95d4
Kätlin <kpadesaar@gmail.com>
2023-02-15 18:19:06 +0200

    Added angular modules Bug Fix

--- src/app/student-module/student-module.module.ts
+++ src/app/student-module/student-module.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {StudentComponent} from "./student/student.component";
+import {StudentFormComponent} from "./student-form/student-form.component";
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    StudentComponent,
+    StudentFormComponent
+  ],
   imports: [
     CommonModule
   ]
--- src/app/teacher-module/teacher-module.module.ts
+++ src/app/teacher-module/teacher-module.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {TeacherComponent} from "./teacher/teacher.component";
+import {TeacherFormComponent} from "./teacher-form/teacher-form.component";
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    TeacherComponent,
+    TeacherFormComponent
+  ],
   imports: [
     CommonModule
   ]
